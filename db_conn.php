a1a89b4e1b7ee71cf2b7ca2bf84bb01b01f92ef4
nick-provost <60225475+nick-provost@users.noreply.github.com>
2021-04-11 23:04:29 -0400

    video descriptions

--- db_conn.php
+++ db_conn.php
@@ -9,5 +9,5 @@ $db_name = "test_db";
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if(!$conn){
-    echo "Connection Failed!";
+    die("Connection failed: ".mysqli_connect_error());
 }
