2964b6412e3565edd1cc669cb98b8fec5c81a680
ColtonS <colton3@g.clemson.edu>
2021-04-15 11:13:00 -0400

    merged projects
    merged features

--- home.php
+++ home.php
@@ -6,80 +6,75 @@ if(isset($_POST['but_upload'])){
    $maxsize = 10485760; // 10MB
   
    if(isset($_FILES['file']['name']) && $_FILES['file']['name'] != ''){
-       $name = $_FILES['file']['name'];
-       $target_dir = "videos/";
-       $target_file = $target_dir . $_FILES["file"]["name"];
-       $uploader = $_SESSION['name'];
-       $title = $_POST['title'];
-       $description = $_POST['description'];
-       $keywords = $_POST['keywords'];
-       $category = $_POST['category'];
+      $name = $_FILES['file']['name'];
+      $target_dir = "videos/";
+      $target_file = $target_dir . $_FILES["file"]["name"];
+      $uploader = $_SESSION['name'];
+      $title = $_POST['title'];
+      $description = $_POST['description'];
+      $keywords = $_POST['keywords'];
+      $category = $_POST['category'];
 
-       // Select file type
-       $extension = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+      // Select file type
+      $extension = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-       // Valid file extensions
-       $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
+      // Valid file extensions
+      $extensions_arr = array("mp4","avi","3gp","mov","mpeg");
 
-       // Check extension
-       if( in_array($extension,$extensions_arr) ){
- 
-          // Check file size
-          if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
-             $_SESSION['message'] = "File too large. File must be less than 10MB.";
-          }else{
-             // Upload
-             if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
-                // Insert record
+      // Check extension
+      if( in_array($extension,$extensions_arr) ){
+         // Check file size
+         if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
+            $_SESSION['message'] = "File too large. File must be less than 10MB.";
+         }else{
+            // Upload
+            if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
+               // Insert record
                $query = "INSERT INTO videos(name,location,title,description,keywords,category) VALUES('".$name."','".$target_file."','".$title."','".$description."','".$keywords."','".$category."')";
-
-
-       // Check extension
-       if( in_array($extension,$extensions_arr) ){
+               
+               // Check extension
+               if( in_array($extension,$extensions_arr) ){
  
-          // Check file size
-          if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
-             $_SESSION['message'] = "File too large. File must be less than 10MB.";
-          }else{
-             // Upload
-             if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
-                // Insert record
-               $duplicates = "select * from videos where name = '$name'";
-               $dups = mysqli_query($conn, $duplicates);
-               if(mysqli_num_rows($dups) === 0){
-                    $query = "INSERT INTO videos(uploader,name,location,title,description,keywords,category) VALUES('".$uploader."','".$name."','".$target_file."','".$title."','".$description."','".$keywords."','".$category."')";
-
-                    mysqli_query($conn,$query);
-                    $_SESSION['message'] = "Upload successfully.";
-                               
+                  // Check file size
+                  if(($_FILES['file']['size'] >= $maxsize) || ($_FILES["file"]["size"] == 0)) {
+                     $_SESSION['message'] = "File too large. File must be less than 10MB.";
+                  }else{
+                     // Upload
+                     if(move_uploaded_file($_FILES['file']['tmp_name'],$target_file)){
+                        // Insert record
+                        $duplicates = "select * from videos where name = '$name'";
+                        $dups = mysqli_query($conn, $duplicates);
+                           if(mysqli_num_rows($dups) === 0){
+                              $query = "INSERT INTO videos(uploader,name,location,title,description,keywords,category) VALUES('".$uploader."','".$name."','".$target_file."','".$title."','".$description."','".$keywords."','".$category."')";
+                              mysqli_query($conn,$query);
+                              $_SESSION['message'] = "Upload successfully.";    
+                           }
+                           else {
+                              echo "A video with that filename has already been uploaded. Rename the file and try again.";
+                           }
+               
+                           $query = "INSERT INTO videos(name,location,title,description,keywords,category) VALUES('".$name."','".$target_file."','".$title."','".$description."','".$keywords."','".$category."')";
+                           mysqli_query($conn,$query);
+                           $_SESSION['message'] = "Upload successfully.";               
+                     }    
+                  }
+                  
+                  $query = "INSERT INTO videos(name,location) VALUES('".$name."','".$target_file."')";
+                  mysqli_query($conn,$query);
+                  $_SESSION['message'] = "Upload successfully.";             
+               }         
+               else{
+                  $_SESSION['message'] = "Invalid file extension.";
                }
-               else {
-                      echo "A video with that filename has already been uploaded. Rename the file and try again.";
-               }
-               $query = "INSERT INTO videos(name,location,title,description,keywords,category) VALUES('".$name."','".$target_file."','".$title."','".$description."','".$keywords."','".$category."')";
-
-               mysqli_query($conn,$query);
-               $_SESSION['message'] = "Upload successfully.";
-                               
-             }
-          }
-               $query = "INSERT INTO videos(name,location) VALUES('".$name."','".$target_file."')";
-
-               mysqli_query($conn,$query);
-               $_SESSION['message'] = "Upload successfully.";
-                               
-             }
-          }
-
-       }else{
-          $_SESSION['message'] = "Invalid file extension.";
-       }
-   }else{
-       $_SESSION['message'] = "Please select a file.";
-   }
+      }else{
+         $_SESSION['message'] = "Please select a file.";
+      }
    header('location: home.php');
    exit;
 } 
+      }
+   }
+}
  ?>
 <!DOCTYPE html>
 <html>
